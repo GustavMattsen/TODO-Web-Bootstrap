f13ae732f4d94f02f4e828180581a35cb1be2204
Gustav Mattsén <mattsen03@gmail.com>
2025-10-30 10:46:01 +0100

    added more const for personselect and todolist

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const form = document.querySelector("form");
 const titleInput = form.querySelector('input[type="text"]');
 const descInput = form.querySelector("textarea");
 const dateInput = form.querySelector('input[type="date"]');
+const personSelect = form.querySelector("select");
+const todoList = document.querySelector(".card-body");
