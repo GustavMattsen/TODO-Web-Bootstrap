bb885fc935fb6fd83335d50acbca59c9f79b2ad4
Gustav Mattsén <mattsen03@gmail.com>
2025-10-30 13:00:26 +0100

    added array function and created new todo object

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ const descInput = form.querySelector("textarea");
 const dateInput = form.querySelector('input[type="date"]');
 const personSelect = form.querySelector("select");
 const todoList = document.querySelector(".card-body");
+
+// --- A list to store all todos ---
+let todos = [];
+
+// --- When the form is submitted ---
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); // Stop the page from reloading
+
+  // Create a new todo object
+    const newTodo = {
+      title: titleInput.value,
+      description: descInput.value,
+      dueDate: dateInput.value,
+      person:
+        personSelect.value === "-- Select Person (Optional) --"
+          ? ""
+          : personSelect.value,
+      createdDate: new Date().toISOString().split("T")[0],
+    };
+  });
