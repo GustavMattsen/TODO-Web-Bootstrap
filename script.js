d5ec539892af5170042647fa5dc84ee4d46fd2da
Gustav Mattsén <mattsen03@gmail.com>
2025-10-30 10:41:38 +0100

    added new queryselector

--- script.js
+++ script.js
@@ -2,3 +2,4 @@
 const form = document.querySelector("form");
 const titleInput = form.querySelector('input[type="text"]');
 const descInput = form.querySelector("textarea");
+const dateInput = form.querySelector('input[type="date"]');
